0d4c59cc7d00195d94c94dd9712452ea7207f5cf
VarNotUsed <varnotused@protonmail.com>
2024-08-19 18:40:38 +0200

    dashboard overhaul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,114 +17,137 @@ import {
   TableRow,
 } from "@/components/ui/Table";
 
-import { PlusIcon, VoteIcon } from "lucide-react";
-import { useUserMultisigs } from "@/hooks/useMultisig";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/Carousel";
+
+import { VoteIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { usePersistStore } from "@/state/store";
+import { getUserMultisigs } from "@/lib/deploy";
 
 export default function Home() {
   const router = useRouter();
+  const store = usePersistStore();
 
-  const [msigData, setMsigData] = useState<any>();
+  const [multisigEntries, setMultisigEntries] = useState<any[]>([]);
+  const [proposalEntries, setProposalEntries] = useState<any[]>([]);
 
   const notification = {
     title: "There are 4 new proposals to review!",
     description: "1 hour ago",
   };
 
-  const _msigData = useUserMultisigs();
+  const init = async () => {
+    if (!store.wallet.address) return;
 
-  const setData = async () => {
-    setMsigData(await _msigData);
+    const _msigs = await getUserMultisigs(store.wallet.address);
+    setMultisigEntries(_msigs);
   };
+
   useEffect(() => {
-    setData();
-  }, [_msigData]);
+    init();
+  }, [store.wallet.address]);
+
+  useEffect(() => {
+    if(!multisigEntries.length) return;
+
+    const _proposalEntries: any[] = [];
+
+    multisigEntries.forEach((msig: any) => {
+      msig.proposals.forEach((proposal: any) => {
+        _proposalEntries.push({...proposal, multisigAddress: msig.address});
+      });
+    });
+
+    console.log(_proposalEntries)
+    setProposalEntries(_proposalEntries);
+  }, [multisigEntries]);
 
   return (
     <>
-      <p className="text-2xl font-semibold">My Sorosigs</p>
-      <div className="grid grid-cols-1 md:grid-cols-10 gap-4">
-        {msigData &&
-          msigData.map((msig: any) => (
-            <div
-              key={msig.info.title}
-              onClick={() => {router.push(`/multisigs/${msig.address}`)}}
-              className="grid grid-cols-1 gap-4 md:grid-cols-2 mt-4 col-span-1 md:col-span-7"
-            >
-              <Card className="cursor-pointer">
-                <CardHeader>
-                  <CardTitle>{msig.info.name}</CardTitle>
-                  <CardDescription>{msig.info.description}</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <div>
-                    <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                      <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                      <div className="space-y-1">
-                        <p className="text-sm font-medium leading-none">
-                          {notification.title}
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          {notification.description}
-                        </p>
-                      </div>
-                    </div>
+      <div className="px-8 mb-8">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full"
+        >
+          <CarouselContent>
+            {multisigEntries &&
+              multisigEntries.map((msig: any, index: number) => (
+                <CarouselItem
+                  key={index}
+                  className="sm:basis-full md:basis-1/2 lg:basis-1/4"
+                  onClick={() => {
+                    router.push(`/multisigs/${msig.address}`);
+                  }}
+                >
+                  <div className="p-1">
+                    <Card className="cursor-pointer">
+                      <CardHeader>
+                        <CardTitle>{msig.info.name}</CardTitle>
+                        <CardDescription>
+                          {msig.info.description}
+                        </CardDescription>
+                      </CardHeader>
+                    </Card>
                   </div>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <CardTitle>Need another Sorosig?</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <div className="flex items-center">
-                    <PlusIcon />
-                    <p>Create new multisig!</p>
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          ))}
+                </CarouselItem>
+              ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
 
-        <div className="col-span-1 md:col-span-3">
-          <Card>
-            <CardHeader>
-              <CardTitle>New Proposals</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableCaption>A list of your unvoted proposals.</TableCaption>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="w-[100px]">Multisig</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Title</TableHead>
-                    <TableHead className="text-right">Vote</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className="font-medium">
-                      My awesome Sorosig
-                    </TableCell>
-                    <TableCell>Pending</TableCell>
-                    <TableCell>How many eggs should we cook tonight?</TableCell>
-                    <TableCell className="text-right">
-                      <VoteIcon />
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-                <TableFooter>
-                  <TableRow>
-                    <TableCell colSpan={3}>Total Unvoted</TableCell>
-                    <TableCell className="text-right">1</TableCell>
-                  </TableRow>
-                </TableFooter>
-              </Table>
-            </CardContent>
-          </Card>
-        </div>
+      <div className="col-span-1 md:col-span-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>New Proposals</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableCaption>A list of your unvoted proposals.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Multisig</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Title</TableHead>
+                  <TableHead className="text-right">Vote</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {proposalEntries &&
+                  proposalEntries.map((proposal: any, index: number) => (
+                    <TableRow key={index} onClick={() => {router.push(`/multisigs/${proposal.multisigAddress}/proposals/${Number(proposal.id)}`)}}>
+                      <TableCell className="font-medium">
+                        {proposal.proposal.values[0].title}
+                      </TableCell>
+                      <TableCell>{proposal.status.tag}</TableCell>
+                      <TableCell>
+                      {proposal.proposal.values[0].description}
+                      </TableCell>
+                      <TableCell className="flex justify-end">
+                        <VoteIcon />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell colSpan={3}>Total Unvoted</TableCell>
+                  <TableCell className="text-right">1</TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </CardContent>
+        </Card>
       </div>
     </>
   );
