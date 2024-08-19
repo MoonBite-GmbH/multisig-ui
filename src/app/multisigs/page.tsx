4d95100916e9b30d3bfa36f2d602dd9b5b88aea7
VarNotUsed <varnotused@protonmail.com>
2024-08-19 18:41:07 +0200

    display cards for multisigs

--- src/app/multisigs/page.tsx
+++ src/app/multisigs/page.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/Button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+import { Pencil } from "lucide-react";
+
 import { getUserMultisigs } from "@/lib/deploy";
 import { usePersistStore } from "@/state/store";
 import { useRouter } from "next/navigation";
@@ -12,10 +22,12 @@ export default function MultisigsPage() {
   const [entries, setEntries] = useState<any[]>([]);
 
   const init = async () => {
-    if(!store.wallet.address) return;
+    if (!store.wallet.address) return;
 
     const _msigs = await getUserMultisigs(store.wallet.address);
     setEntries(_msigs);
+
+    console.log(_msigs)
   };
 
   useEffect(() => {
@@ -24,10 +36,24 @@ export default function MultisigsPage() {
 
   return (
     <>
+      <Button
+        onClick={() => {
+          router.push("/multisigs/create");
+        }}
+      >
+        Create Multisig
+      </Button>
       <h1 className="text-2xl font-semibold mb-4">Multisigs</h1>
-      {entries.map((entry: any, index: number) => (
-        <div key={index}>{entry.info.name}</div>
-      ))}
+      <div className="grid grid-cols-5 gap-4">
+        {entries.map((entry: any, index: number) => (
+          <Card key={index} className="cursor-pointer" onClick={() => {router.push(`/multisigs/${entry.address}`)}}>
+            <CardHeader>
+              <CardTitle>{entry.info.name}</CardTitle>
+              <CardDescription>{entry.info.description}</CardDescription>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
     </>
   );
 }
