cc182e0b392561c0911334c665f4d8197c2af039
VarNotUsed <varnotused@protonmail.com>
2024-08-21 14:56:56 +0200

    added package to truncate texts on second line

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,7 +78,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/line-clamp')],
 } satisfies Config
 
 export default config
