d32c20cafe42066f629ef55110b350841be68dcc
VarNotUsed <varnotused@protonmail.com>
2024-08-22 17:13:27 +0200

    dashboard icons

--- src/app/multisigs/page.tsx
+++ src/app/multisigs/page.tsx
@@ -8,7 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/Card";
-import { Pencil } from "lucide-react";
+import { Pencil, UserIcon, VoteIcon } from "lucide-react";
 
 import { getUserMultisigs } from "@/lib/deploy";
 import { usePersistStore } from "@/state/store";
@@ -27,7 +27,7 @@ export default function MultisigsPage() {
     const _msigs = await getUserMultisigs(store.wallet.address);
     setEntries(_msigs);
 
-    console.log(_msigs)
+    console.log(_msigs);
   };
 
   useEffect(() => {
@@ -36,20 +36,42 @@ export default function MultisigsPage() {
 
   return (
     <>
-      <Button
-        onClick={() => {
-          router.push("/multisigs/create");
-        }}
-      >
-        Create Multisig
-      </Button>
-      <h1 className="text-2xl font-semibold mb-4">Multisigs</h1>
-      <div className="grid grid-cols-5 gap-4">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold mb-4">Multisigs</h1>
+        <Button
+          onClick={() => {
+            router.push("/multisigs/create");
+          }}
+        >
+          Create Multisig
+        </Button>
+      </div>
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-4">
         {entries.map((entry: any, index: number) => (
-          <Card key={index} className="cursor-pointer" onClick={() => {router.push(`/multisigs/${entry.address}`)}}>
+          <Card
+            key={index}
+            className="cursor-pointer"
+            onClick={() => {
+              router.push(`/multisigs/${entry.address}`);
+            }}
+          >
             <CardHeader>
-              <CardTitle>{entry.info.name}</CardTitle>
-              <CardDescription>{entry.info.description}</CardDescription>
+              <CardTitle className="truncate mb-2">{entry.info.name}</CardTitle>
+              <CardDescription className="line-clamp-2 h-11">
+                {entry.info.description}
+              </CardDescription>
+              <CardContent className="pb-0 pt-4">
+                <div className="flex justify-around">
+                  <div className="flex">
+                    <UserIcon width={20} />
+                    <p className="ml-1">{entry.members.length}</p>
+                  </div>
+                  <div className="flex">
+                    <VoteIcon width={20} />
+                    <p className="ml-1">{entry.proposals.length}</p>
+                  </div>
+                </div>
+              </CardContent>
             </CardHeader>
           </Card>
         ))}
