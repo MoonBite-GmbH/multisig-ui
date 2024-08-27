8d0e01b8e9bdfeb0cea306ebfa64a66da09a47a1
VarNotUsed <varnotused@protonmail.com>
2024-08-27 16:39:06 +0200

    new deployer contract address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -17,11 +17,6 @@ export const NavItems: NavItem[] = [
   },
 ];
 
-export const TESTING_SOURCE: Account = new Account(
-  "GBUHRWJBXS4YAEOVDRWFW6ZC5LLF2SAOMATH4I6YOTZYHE65FQRFOKG2",
-  "1",
-);
-
 /**
  * The Soroban network passphrase used to initialize this library.
  */
@@ -34,10 +29,5 @@ export const RPC_URL: string =
   "https://soroban-testnet.stellar.org/";
 
 export const DEPLOY_CONTRACT_ID: string =
-  "CAERBL6MW3K5DWZQWLPV24PLAD63IBWZ5QKABHPUWCMM4IMMGV6WPMZG";
+  "CAAJNRGWI6HZLN56SVBTMTZV3VIY72A4CLCX4D4DMGE6C2ACFRPOWSZL";
 
-// TODO: Fetch multisigs from the api, for now we will use this hardcoded list
-export const MULTISIGS = [
-  "CCZ7IPQPGPV4JF6TROB7MWEAFBWEQDOLXNFIN3XAXT3JZR3EVCWJ77AW",
-  "CCJT7H7JZ5EDV5FIJMKKGBVBOR2OGEURELD7HINKGTKNOBRW6HHR5WNU"
-];
