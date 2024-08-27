50a85131fd429ce9aa221f7d3e5fcae3dad28991
VarNotUsed <varnotused@protonmail.com>
2024-08-27 17:28:58 +0200

    expiration date in both proposal creation functions

--- src/lib/multisig.ts
+++ src/lib/multisig.ts
@@ -16,12 +16,14 @@ export const createTransactionProposal = async (
     recipient,
     amount,
     token,
+    expiration_date,
   }: {
     title: string;
     description: string;
     recipient: string;
     amount: number;
     token: string;
+    expiration_date: Date | undefined;
   },
 ) => {
   // Create Client
@@ -41,6 +43,42 @@ export const createTransactionProposal = async (
       recipient,
       amount: BigInt(amount),
       token,
+      expiration_date: expiration_date ? BigInt(expiration_date.getTime()) : undefined,
+    });
+
+    const res = await tx.signAndSend();
+    return res.result.unwrap();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createUpdateProposal = async (
+  wallet: Wallet,
+  userPublicKey: string,
+  contractId: string,
+  {
+    wasmHash,
+    expiration_date
+  }: {
+    wasmHash: string;
+    expiration_date: Date | undefined;
+  },
+) => {
+  // Create Client
+  const client = new MultisigContract.Client({
+    publicKey: userPublicKey,
+    signTransaction: (tx: string) => wallet.signTransaction(tx),
+    contractId: contractId,
+    networkPassphrase: NETWORK_PASSPHRASE,
+    rpcUrl: RPC_URL,
+  });
+
+  try {
+    const tx = await client.create_update_proposal({
+      sender: userPublicKey,
+      new_wasm_hash: Buffer.from(wasmHash),
+      expiration_date: expiration_date ? BigInt(expiration_date.getTime()) : undefined,
     });
 
     const res = await tx.signAndSend();
