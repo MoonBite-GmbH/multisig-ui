2455958559da08e2fcd35ce141458390aedabfad
VarNotUsed <varnotused@protonmail.com>
2024-09-05 13:52:24 +0200

    skeletons multisigs

--- src/app/multisigs/page.tsx
+++ src/app/multisigs/page.tsx
@@ -14,6 +14,7 @@ import { getUserMultisigs } from "@/lib/deploy";
 import { usePersistStore } from "@/state/store";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/Skeleton";
 
 export default function MultisigsPage() {
   const store = usePersistStore();
@@ -45,34 +46,46 @@ export default function MultisigsPage() {
         </Button>
       </div>
       <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-4">
-        {entries.map((entry: any, index: number) => (
-          <Card
-            key={index}
-            className="cursor-pointer"
-            onClick={() => {
-              router.push(`/multisigs/${entry.address}`);
-            }}
-          >
-            <CardHeader>
-              <CardTitle className="truncate mb-2">{entry.info.name}</CardTitle>
-              <CardDescription className="line-clamp-2 h-11">
-                {entry.info.description}
-              </CardDescription>
-              <CardContent className="pb-0 pt-4">
-                <div className="flex justify-around">
-                  <div className="flex">
-                    <UserIcon width={20} />
-                    <p className="ml-1">{entry.members.length}</p>
+        {entries?.length ? (
+          entries.map((entry: any, index: number) => (
+            <Card
+              key={index}
+              className="cursor-pointer"
+              onClick={() => {
+                router.push(`/multisigs/${entry.address}`);
+              }}
+            >
+              <CardHeader>
+                <CardTitle className="truncate mb-2">
+                  {entry.info.name}
+                </CardTitle>
+                <CardDescription className="line-clamp-2 h-11">
+                  {entry.info.description}
+                </CardDescription>
+                <CardContent className="pb-0 pt-4">
+                  <div className="flex justify-around">
+                    <div className="flex">
+                      <UserIcon width={20} />
+                      <p className="ml-1">{entry.members.length}</p>
+                    </div>
+                    <div className="flex">
+                      <VoteIcon width={20} />
+                      <p className="ml-1">{entry.proposals.length}</p>
+                    </div>
                   </div>
-                  <div className="flex">
-                    <VoteIcon width={20} />
-                    <p className="ml-1">{entry.proposals.length}</p>
-                  </div>
-                </div>
-              </CardContent>
-            </CardHeader>
-          </Card>
-        ))}
+                </CardContent>
+              </CardHeader>
+            </Card>
+          ))
+        ) : (
+          <>
+            <Skeleton className="h-[168px] w-full rounded-xl" />
+            <Skeleton className="h-[168px] w-full rounded-xl" />
+            <Skeleton className="h-[168px] w-full rounded-xl" />
+            <Skeleton className="h-[168px] w-full rounded-xl" />
+            <Skeleton className="h-[168px] w-full rounded-xl" />
+          </>
+        )}
       </div>
     </>
   );
