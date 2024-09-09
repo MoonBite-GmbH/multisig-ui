481fa72c41ec60cac3f951b7a54d41b2ec668aff
VarNotUsed <varnotused@protonmail.com>
2024-09-09 16:52:44 +0200

    standalone network setting

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -23,7 +23,7 @@ export const createConnectWalletActions = () => {
       // Get the network details from the user's wallet.
       // TODO: Make this dynamic
       const networkDetails = {
-        network: "MAINNET",
+        network: "STANDALONE",
         networkPassphrase: NETWORK_PASSPHRASE,
         networkUrl: RPC_URL,
         sorobanRpcUrl: RPC_URL,
